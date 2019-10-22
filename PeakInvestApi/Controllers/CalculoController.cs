7539c35bc465d008b068599bdf5160a6f4e09bbf
Braian Pavanelli <braian.pavanelli@consultsolucoes.com.br>
2019-10-22 09:57:00 -0300

    Api

---
+++ PeakInvestApi/Controllers/CalculoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace PeakInvestApi.Controllers
+{
+    [AllowAnonymous]
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class CalculoController : ApiController
+    {
+        // GET: api/Calculo
+        [HttpGet]
+        public double Get(double valor, int numParcelas)
+        {
+            return (valor * numParcelas) * 1.05;
+        }
+    }
+}
---
+++ PeakInvestApi/Controllers/ConsultaUsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace PeakInvestApi.Controllers
+{
+    [AllowAnonymous]
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class ConsultaUsuarioController : ApiController
+    {
+        // GET: api/ConsultaUsuario
+        [HttpGet]
+        public IEnumerable<KeyValuePair<int, string>> Get()
+        {
+            var lista = new List<KeyValuePair<int, string>>();
+            lista.Add(new KeyValuePair<int, string>(1, "João"));
+            lista.Add(new KeyValuePair<int, string>(2, "Maria"));
+            lista.Add(new KeyValuePair<int, string>(3, "Ana"));
+
+            return lista;
+        }
+
+        // GET: api/ConsultaUsuario/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            var lista = new List<KeyValuePair<int, string>>();
+            lista.Add(new KeyValuePair<int, string>(1, "João"));
+            lista.Add(new KeyValuePair<int, string>(2, "Maria"));
+            lista.Add(new KeyValuePair<int, string>(3, "Ana"));
+
+            return lista.Where(l => l.Key == id).SingleOrDefault().Value;
+        }
+    }
+}
